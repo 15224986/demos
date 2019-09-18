f33ef92dd1c6eb4a63196a950512c2124babbac2
15224986 <35017110+15224986@users.noreply.github.com>
2019-09-18 14:06:03 +0800

    Delete index.js

--- components/vant-field-date/index.js
+++
@@ -1,6 +0,0 @@
-import vantFieldDate from './src/vant-field-date';
-/* istanbul ignore next */
-vantFieldDate.install = function(Vue) {
-	Vue.component(vantFieldDate.componentName, vantFieldDate);
-};
-export default vantFieldDate;
